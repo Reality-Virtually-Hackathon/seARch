2962b2fefd1491e0ff308c5cf567d6e3d77026a9
Wallace Lages <wallace.lages@gmail.com>
2017-10-08 16:17:12 -0400

    Adding Helmet model

---
+++ Source/Unity3D/Assets/Scripts/OnlyServer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnlyServer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!AppStateManager.Instance.isCommandCenter)
+            this.gameObject.SetActive(false);
+        else
+            this.gameObject.SetActive(true);
+
+    }
+}
